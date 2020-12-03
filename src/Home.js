1483917707dc0d533de2cf156a0b764448d67536
Brady Jones <brady.jones@hotmail.co.uk>
2020-12-03 10:42:06 +0000

    moved Home.js to src folder

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./Home.css";
+import { Link } from "react-router-dom";
+
+class Home extends Component {
+	render() {
+		return (
+			<div> 
+			</div>
+		);
+	}
+}
+
+export default Home;
