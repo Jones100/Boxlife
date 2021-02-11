a4436fe0033137ea6034d876ff0da5f7013607eb
Brady Jones <brady.jones@hotmail.co.uk>
2021-02-11 12:09:32 +0000

    state toggle onclick

--- src/Stopwatch.js
+++ src/Stopwatch.js
@@ -6,11 +6,20 @@ class StopWatch extends Component {
 	state = {
 		isRunning : false
 	}
+
+	handleStopwatch = () => {
+		this.setState({
+			isRunning : !this.state.isRunning
+		});
+	}
+
 	render() {
 		return (
 			<div>
 				<h2 className = "stopwatch"> Stopwatch </h2>
-				<button> {this.state.isRunning ? "Stop" : "Start"} </button>
+				<button onClick = {this.handleStopwatch}> 
+					{this.state.isRunning ? "Stop" : "Start"} 
+				</button>
 				<button> Reset </button>
 			</div>
 		)
